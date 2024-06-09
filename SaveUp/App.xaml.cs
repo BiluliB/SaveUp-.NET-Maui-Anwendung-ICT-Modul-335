7c78b23204d100c1859150d4470c200852a4227c
Bobby <138381572+BiluliB@users.noreply.github.com>
2024-06-09 19:34:45 +0200

    Add project files.

---
+++ SaveUp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SaveUp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SaveUp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SaveUp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
